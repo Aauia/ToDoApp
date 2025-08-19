a28c4f4c7b396d98ff3d16a05b5baca43f8f83c3
Ayaulym Olzhaskyzy Abduokhapova <156919123+Aauia@users.noreply.github.com>
2025-08-19 19:23:54 +0500

    Update BaseProtocols.swift

--- ToDo/VIPER/Base/BaseProtocols.swift
+++ ToDo/VIPER/Base/BaseProtocols.swift
@@ -2,25 +2,25 @@ import Foundation
 import SwiftUI
 import UIKit
 
-// MARK: - Base VIPER Protocols
+
 
 protocol BaseView: AnyObject {
-    // Remove associated types to avoid conformance issues
+
 }
 
 protocol BasePresenter: AnyObject {
-    // Remove associated types to avoid conformance issues
+
 }
 
 protocol BaseInteractor: AnyObject {
-    // Remove associated types to avoid conformance issues
+
 }
 
 protocol BaseRouter: AnyObject {
-    // Base router protocol for VIPER architecture
+
 }
 
-// MARK: - Entity Protocol
+
 
 protocol BaseEntity {
     var id: String { get }
